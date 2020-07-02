b47ea3b713b568b6ac13d5027a74b3361e03c652
Andrei Sasu <andrei.sasu42@gmail.com>
2020-07-02 08:09:21 +0100

    Add Javadoc

--- ChessProject-Java/src/main/java/com/solarwindsmsp/chess/movement/MovementStrategy.java
+++ ChessProject-Java/src/main/java/com/solarwindsmsp/chess/movement/MovementStrategy.java
@@ -3,5 +3,13 @@ package com.solarwindsmsp.chess.movement;
 import com.solarwindsmsp.chess.MovementType;
 
 public interface MovementStrategy {
+    /**
+     * Attempts to perform a movement of a Piece according to the strategy for that Piece
+     * This method will do nothing if the Movement type or new coordinates are invalid for that Piece.
+     *
+     * @param movementType the {@link MovementType}
+     * @param xCoordinate the new xCoordinate
+     * @param yCoordinate the new yCoordinate
+     */
     void move(final MovementType movementType, final int xCoordinate, final int yCoordinate);
 }
